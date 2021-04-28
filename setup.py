0e9d3b182302e24bbcc37d161f11577dd0510c53
Hidden <hiddengob@free.fr>
2021-04-28 21:50:42 +0200

    Fix jar path

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="eaiscenarioreporter",
-    version="0.0.13",
+    version="0.0.14",
     description="Turns folder of gherkin feature files into a docx file.",
     long_description=long_description,
     long_description_content_type="text/markdown",
